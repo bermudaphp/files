dcaede71cbd4ef6ffb17a917d72b82d5ecb52743
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-10-01 19:13:54 +0200

    Update FileUploadHandler.php

--- src/FileUploadHandler.php
+++ src/FileUploadHandler.php
@@ -2,28 +2,29 @@
 
 namespace Bermuda\Flysystem;
 
-use Bermuda\Utils\Header;
+use Bermuda\Flysystem\Validation\UploadedFileValidationExtension;
+use Bermuda\Flysystem\Validation\UploadedFileValidator;
+use Bermuda\Flysystem\Validation\UploadedFileValidatorInterface;
 use Bermuda\String\Json;
+use Bermuda\Utils\Header;
 use Bermuda\Utils\Types\Application;
-use Bermuda\Utils\Types\Text;
 use Bermuda\Utils\Types\Image as ImageType;
+use Bermuda\Utils\Types\Text;
+use League\Flysystem\FilesystemException;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\UploadedFileInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Bermuda\Flysystem\Validation\UploadedFileValidator;
-use Bermuda\Flysystem\Validation\UploadedFileValidatorInterface;
-use Bermuda\Flysystem\Validation\UploadedFileValidationExtension;
 
 final class FileUploadHandler implements RequestHandlerInterface, FileProcessor
 {
     private Location $location;
 
     public function __construct(
-        private ResponseFactoryInterface $responseFactory,
-        private ?Flysystem $flysystem = null,
+        private ResponseFactoryInterface        $responseFactory,
+        private ?Flysystem                      $flysystem = null,
         private ?UploadedFileValidatorInterface $validator = null
     )
     {
@@ -41,20 +42,6 @@ final class FileUploadHandler implements RequestHandlerInterface, FileProcessor
     }
 
     /**
-     * @param string|null $dir
-     * @return string
-     */
-    public function tmpDir(?string $dir = null): string
-    {
-        if ($dir != null)
-        {
-            $this->location = new Location($dir);
-        }
-
-        return $this->location;
-    }
-
-    /**
      * @param ContainerInterface $container
      * @return static
      */
@@ -69,23 +56,32 @@ final class FileUploadHandler implements RequestHandlerInterface, FileProcessor
     }
 
     /**
+     * @param string|null $dir
+     * @return string
+     */
+    public function tmpDir(?string $dir = null): string
+    {
+        if ($dir != null) {
+            $this->location = new Location($dir);
+        }
+
+        return $this->location;
+    }
+
+    /**
      * @param ServerRequestInterface $request
      * @return ResponseInterface
-     * @throws \League\Flysystem\FilesystemException
+     * @throws FilesystemException
      */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $files = $request->getUploadedFiles();
         $filesIDs = [];
 
-        try
-        {
-            foreach ($files as $file)
-            {
-                if (is_array($file))
-                {
-                    foreach ($file as $value)
-                    {
+        try {
+            foreach ($files as $file) {
+                if (is_array($file)) {
+                    foreach ($file as $value) {
                         $filesIDs[] = $this->processFile($this->location, $value);
                     }
 
@@ -94,15 +90,11 @@ final class FileUploadHandler implements RequestHandlerInterface, FileProcessor
 
                 $filesIDs[] = $this->processFile($this->location, $file);
             }
-        }
-
-        catch (UploadedFileValidationExtension $e)
-        {
+        } catch (UploadedFileValidationExtension $e) {
             return $this->handleValidationException($e);
         }
 
-        if (count($filesIDs) > 1)
-        {
+        if (count($filesIDs) > 1) {
             $contentType = Application::json;
             $filesIDs = Json::encode($filesIDs);
         }
@@ -119,23 +111,33 @@ final class FileUploadHandler implements RequestHandlerInterface, FileProcessor
      * @param string $path
      * @param UploadedFileInterface $uploadedFile
      * @return string
-     * @throws \League\Flysystem\FilesystemException
+     * @throws FilesystemException
      */
     public function processFile(string $location, UploadedFileInterface $uploadedFile): string
     {
         $this->validator->validate($uploadedFile);
 
         $filename = (new Location($location))->append($uploadedFile->getClientFilename());
-        $this->flysystem->write($filename, (string) $uploadedFile->getStream());
+        $this->flysystem->write($filename, (string)$uploadedFile->getStream());
 
         return $uploadedFile->getClientFilename();
+    }
+
+    private function handleValidationException(UploadedFileValidationExtension $e): ResponseInterface
+    {
+        ($response = $this->responseFactory->createResponse(400)
+            ->withHeader(Header::contentType, Application::json))
+            ->getBody()
+            ->write(json_encode(['errors' => $e->getErrors()]));
+
+        return $response;
     }
 
     /**
      * @param string $location
      * @param string[]|string $filesIDs
      * @return File[]
-     * @throws \League\Flysystem\FilesystemException
+     * @throws FilesystemException
      */
     public function moveUploadedFiles(string $location, array|string $filesIDs): array
     {
@@ -145,22 +147,11 @@ final class FileUploadHandler implements RequestHandlerInterface, FileProcessor
 
         $this->flysystem->exists($location) ?: $this->flysystem->createDirectory($location);
 
-        foreach ($filesIDs as $fileID)
-        {
-            ($files[] =$this->flysystem->openFile($this->location->append($fileID)))
+        foreach ($filesIDs as $fileID) {
+            ($files[] = $this->flysystem->openFile($this->location->append($fileID)))
                 ->move($location);
         }
 
         return $files;
-    }
-
-    private function handleValidationException(UploadedFileValidationExtension $e): ResponseInterface
-    {
-        ($response = $this->responseFactory->createResponse(400)
-            ->withHeader(Header::contentType, Application::json))
-        ->getBody()
-            ->write(json_encode(['errors' => $e->getErrors()]));
-
-        return $response;
     }
 }
